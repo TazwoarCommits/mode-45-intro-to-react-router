8031004e96c47e5813cc1e86a8e6badcc4518739
TazwoarCommits <tazwoarlives@gmail.com>
2024-10-29 15:22:53 +0600

    45.2+3

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h2 className="text-6xl text-red-800">Navbar</h2>
+            <nav>
+                <a href="/">Home</a>
+                <a href="/about">ABout</a>
+                <a href="/contact">Contact Us</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -5,25 +5,35 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import App from './App.jsx'
+import Home from './Components/Home/Home';
+// import Header from './Components/Header/Header';
+import About from './Components/About/About';
+import Contact from './Components/Contact/Contact';
+// import App from './App.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello From React Router</div>,
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/about',
+
+        element: <About></About>
+      },
+
+      {
+        path: '/contact',
+        element: <Contact></Contact>
+      }
+
+    ]
   },
-  {
-    path: "/about",
-    element: <div className="">I am from the about page</div>
-  },
-  {
-    path: "/contact",
-    element: <div>Call me right Now!!</div>
-  },
+
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-        <RouterProvider router={router} />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
