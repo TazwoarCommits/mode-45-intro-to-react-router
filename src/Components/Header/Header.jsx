df7c4cf46d33ad2f8acf2d9d0ede76e058673822
TazwoarCommits <tazwoarlives@gmail.com>
2024-10-30 22:33:41 +0600

    45.8

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom";
-
+import { Link, NavLink } from "react-router-dom";
+import './Header.css'
 
 const Header = () => {
     return (
@@ -7,9 +7,11 @@ const Header = () => {
             <h2 className="text-6xl text-red-800">Navbar</h2>
             <nav className="mr-5 text-stone-400">
                 <Link className="mr-5" to='/'>Home</Link>
-                <Link className="mr-5" to='/users'>Users</Link>
+                <NavLink className="mr-5" to='/users'>Users</NavLink>
+                {/* <Link className="mr-5" to='/users'>Users</Link> */}
                 <Link className="mr-5" to='/about'>About</Link>
-                <Link className="mr-5" to='/posts'>Posts</Link>
+                <NavLink className="mr-5" to='/posts'>Posts</NavLink>
+                {/* <Link className="mr-5" to='/posts'>Posts</Link> */}
                 <Link className="mr-5" to='/contact'>Contact Us</Link>
             </nav>
         </div>
