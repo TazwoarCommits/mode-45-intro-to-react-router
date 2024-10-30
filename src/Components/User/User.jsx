22745f7d195f7bce39e3b52788b930450f694b66
TazwoarCommits <tazwoarlives@gmail.com>
2024-10-30 14:15:00 +0600

    fixed 45.4

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -3,9 +3,8 @@
 const User = ({user}) => {
     const {name , id , email , phone} = user
     return (
-        <div className="bg-slate-300 my-6
-         rounded-2xl text-black">
-            <div className="m-4">
+        <div className="bg-slate-400 text-black/75 p-8 rounded-2xl">
+            <div className="">
             <p>{id}.</p>
             <h2>Name : {name}</h2>
             <p>Email : {email}</p>
--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -5,12 +5,12 @@ import User from "../User/User";
 const Users = () => {
  
     const ourUsers = useLoaderData() ;
-    console.log(ourUsers);
+    // console.log(ourUsers);
     return (
         <div>
             <h2>Our Users</h2>
             <p>Total Users : {ourUsers.length}</p>
-            <div className="grid grid-cols-3 space-x-4 w-11/12 container mx-auto">
+            <div className="grid grid-cols-3 container mx-auto gap-6">
                 {
                     ourUsers.map((user , idx) => <User key={idx} user={user} ></User>)
                 }
